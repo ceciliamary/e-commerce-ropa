ac0a8e386ae21760207b7c69bb5d98781f5ddb34
ceciliamary <108771205+ceciliamary@users.noreply.github.com>
2024-04-11 12:59:28 -0300

    e-commerce ropa

---
+++ productos.js
@@ -0,0 +1,29 @@
+const productos = [
+    { id: 1,
+    productoNombre: "Remera",
+    precio: 14000, 
+    cantidad: 1,
+    img: "./img/remera.jpg",
+    },
+
+    { id: 2,
+        productoNombre: "Buzo",
+        precio: 34000, 
+        cantidad: 1,
+        img: "./img/buzo.jpg",
+        },
+
+    { id: 3,
+        productoNombre: "Pantalon",
+        precio: 44000, 
+        cantidad: 1,
+        img: "./img/pantalon.jpg",
+        },
+
+    { id: 4,
+        productoNombre: "Short",
+        precio: 24000, 
+        cantidad: 1,
+        img: "./img/short.jpg",
+        },    
+];
